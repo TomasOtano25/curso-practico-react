2364541787b6f782cd081160c832e3db37ac4813
Tomas Garcia <tomasotano25@gmail.com>
2022-10-16 23:50:11 -0400

    clase 11 navegacion entre rutas

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,21 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { NewPassword } from "../containers/NewPassword";
-import { HomePage } from "../containers/HomePage";
+import { HomePage } from "../pages/HomePage";
 import "../styles/global.css";
 import { Layout } from "../containers/Layout";
 import { RecoveryPassword } from "../containers/RecoveryPassword";
-import { NotFound } from "../containers/NotFound";
+import { NotFound } from "../pages/NotFound";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/new-password" element={<NewPassword />} />
-        <Route path="/recovery-password" element={<RecoveryPassword />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/new-password" element={<NewPassword />} />
+          <Route path="/recovery-password" element={<RecoveryPassword />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
