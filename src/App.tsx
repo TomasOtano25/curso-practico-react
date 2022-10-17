afc32d7dfc7957457c3b2623fd0702252cbcec5b
Tomas Garcia <tomasotano25@gmail.com>
2022-10-16 22:49:49 -0400

    clase 08 transformando html y css en componentes de react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import { LoginPage } from "./containers/LoginPage";
+import { LoginForm } from "./components/LoginForm";
+import { HomePage } from "./containers/HomePage";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/login"
+          element={
+            <LoginPage>
+              <LoginForm />
+            </LoginPage>
+          }
+        />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
